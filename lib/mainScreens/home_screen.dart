969d1b76935878300bacea57531b51d9d5b01c1c
alexpays99 <mosisa.alex@gmail.com>
2022-06-29 20:27:27 +0300

    designed menus upload screen

--- lib/mainScreens/home_screen.dart
+++ lib/mainScreens/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:sellers_app/authentication/auth_screen.dart';
 import 'package:sellers_app/global/gloval.dart';
+import 'package:sellers_app/uploadScreens/menus_upload_screen.dart';
+import 'package:sellers_app/widgets/my_drawer.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -13,10 +15,14 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: const MyDrawer(),
       appBar: AppBar(
-        title: Text(sharedPreferences!.getString('uid')!),
+        title: Text(
+          sharedPreferences!.getString('uid')!,
+          style: const TextStyle(fontSize: 30, fontFamily: 'Lobster'),
+        ),
         centerTitle: true,
-        automaticallyImplyLeading: false,
+        automaticallyImplyLeading: true,
         flexibleSpace: Container(
           decoration: const BoxDecoration(
             gradient: LinearGradient(
@@ -31,25 +37,20 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
           ),
         ),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          child: const Text('Logout'),
-          style: ElevatedButton.styleFrom(
-            primary: Colors.cyan,
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (c) => const MenusUploadScreen()));
+            },
+            icon: const Icon(
+              Icons.post_add,
+              color: Color.fromARGB(255, 255, 248, 218),
+            ),
           ),
-          onPressed: () {
-            firebaseAuth.signOut().then((value) {
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(
-                  builder: (c) => const AuthScren(),
-                ),
-              );
-            });
-          },
-        ),
+        ],
       ),
+      body: Center(),
     );
   }
 }
---
+++ lib/uploadScreens/menus_upload_screen.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:sellers_app/global/gloval.dart';
+import 'package:sellers_app/mainScreens/home_screen.dart';
+
+class MenusUploadScreen extends StatefulWidget {
+  const MenusUploadScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MenusUploadScreen> createState() => _MenusUploadScreenState();
+}
+
+class _MenusUploadScreenState extends State<MenusUploadScreen> {
+  defaultScreen() {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Add New Menu',
+          style: TextStyle(fontSize: 30, fontFamily: 'Lobster'),
+        ),
+        centerTitle: true,
+        automaticallyImplyLeading: true,
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.cyan,
+                Colors.amber,
+              ],
+              begin: FractionalOffset(0.0, 0.0),
+              end: FractionalOffset(1.0, 0.0),
+              stops: [0.0, 1.0],
+              tileMode: TileMode.clamp,
+            ),
+          ),
+        ),
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (c) => const HomeScreen()),
+            );
+          },
+        ),
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Colors.cyan,
+              Colors.amber,
+            ],
+            begin: FractionalOffset(0.0, 0.0),
+            end: FractionalOffset(1.0, 0.0),
+            stops: [0.0, 1.0],
+            tileMode: TileMode.clamp,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Icon(
+                Icons.shop_two,
+                color: Colors.white,
+                size: 200,
+              ),
+              ElevatedButton(
+                child: const Text(
+                  'Add New Menu',
+                  style: TextStyle(color: Colors.white, fontSize: 18),
+                ),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.amber),
+                  shape: MaterialStateProperty.all(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return defaultScreen();
+  }
+}
