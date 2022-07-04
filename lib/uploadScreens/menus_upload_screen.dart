c4e69d6a9a3d50d662a912822a9aecb113b375ab
alexpays99 <mosisa.alex@gmail.com>
2022-07-04 23:01:17 +0300

    upload menu finished

--- lib/uploadScreens/menus_upload_screen.dart
+++ lib/uploadScreens/menus_upload_screen.dart
@@ -1,6 +1,6 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:sellers_app/global/gloval.dart';
 import 'package:sellers_app/mainScreens/home_screen.dart';
 
 class MenusUploadScreen extends StatefulWidget {
@@ -13,6 +13,9 @@ class MenusUploadScreen extends StatefulWidget {
 class _MenusUploadScreenState extends State<MenusUploadScreen> {
   XFile? imageXFile;
   final ImagePicker _picker = ImagePicker();
+
+  TextEditingController shortInfoController = TextEditingController();
+  TextEditingController titleController = TextEditingController();
 
   defaultScreen() {
     return Scaffold(
@@ -144,7 +147,7 @@ class _MenusUploadScreenState extends State<MenusUploadScreen> {
 
     imageXFile = await _picker.pickImage(
       source: ImageSource.camera,
-      maxHeight: 680,
+      maxHeight: 720,
       maxWidth: 1280,
     );
 
@@ -167,8 +170,141 @@ class _MenusUploadScreenState extends State<MenusUploadScreen> {
     });
   }
 
+  menuUploadFormScreen() {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'U[ploading] New Menu',
+          style: TextStyle(fontSize: 20, fontFamily: 'Lobster'),
+        ),
+        centerTitle: true,
+        automaticallyImplyLeading: true,
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.cyan,
+                Colors.amber,
+              ],
+              begin: FractionalOffset(0.0, 0.0),
+              end: FractionalOffset(1.0, 0.0),
+              stops: [0.0, 1.0],
+              tileMode: TileMode.clamp,
+            ),
+          ),
+        ),
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+          onPressed: () {
+            //Navigator.push(context,MaterialPageRoute(builder: (c) => const HomeScreen()),);
+            clearMenuUploadForm();
+          },
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {},
+            child: const Text(
+              'Add',
+              style: TextStyle(
+                color: Colors.cyan,
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+                fontFamily: 'Varela',
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: ListView(
+        children: [
+          Container(
+            height: 230,
+            width: MediaQuery.of(context).size.width * 0.8,
+            child: Center(
+              child: AspectRatio(
+                aspectRatio: 16 / 9,
+                child: Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: FileImage(
+                        File(imageXFile!.path),
+                      ),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          const Divider(
+            color: Colors.amber,
+            thickness: 2,
+          ),
+          ListTile(
+            leading:
+                const Icon(Icons.perm_device_information, color: Colors.cyan),
+            title: Container(
+              width: 250,
+              child: TextField(
+                style: const TextStyle(
+                  color: Colors.black,
+                ),
+                controller: shortInfoController,
+                decoration: const InputDecoration(
+                  hintText: 'Menu Info',
+                  hintStyle: TextStyle(
+                    color: Colors.grey,
+                  ),
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+          ),
+          const Divider(
+            color: Colors.amber,
+            thickness: 2,
+          ),
+          ListTile(
+            leading: const Icon(Icons.title, color: Colors.cyan),
+            title: Container(
+              width: 250,
+              child: TextField(
+                style: const TextStyle(
+                  color: Colors.black,
+                ),
+                controller: titleController,
+                decoration: const InputDecoration(
+                  hintText: 'Menu Title',
+                  hintStyle: TextStyle(
+                    color: Colors.grey,
+                  ),
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+          ),
+          const Divider(
+            color: Colors.amber,
+            thickness: 2,
+          ),
+        ],
+      ),
+    );
+  }
+
+  clearMenuUploadForm() {
+    setState(() {
+      shortInfoController.clear();
+      titleController.clear();
+      imageXFile = null;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return defaultScreen();
+    return imageXFile == null ? defaultScreen() : menuUploadFormScreen();
   }
 }
